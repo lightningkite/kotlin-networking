3ff4d4e8b7a88acbc7ab48e1fc6fa5341af12d72
jivie <joseph@lightningkite.com>
2016-06-15 09:42:05 -0600

    Package change

--- src/main/java/com/ivieleague/kotlin/networking/NetMethod.kt
+++
@@ -1,8 +0,0 @@
-package com.ivieleague.kotlin.networking
-
-/**
- * Created by jivie on 1/28/16.
- */
-enum class NetMethod {
-    GET, POST, PUT, PATCH, DELETE
-}
--- src/main/java/com/ivieleague/kotlin/networking/NetStack.kt
+++
@@ -1,9 +0,0 @@
-package com.ivieleague.kotlin.networking
-
-/**
- * An interface that represents something that works like a network stack, be it a real stack or a mock stack.
- * Created by jivie on 1/13/16.
- */
-interface NetStack {
-    fun stream(request: NetRequest): NetStream
-}
